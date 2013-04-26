22466c13c0d93d8a4ac891ff864055f28abfb779
Tom Scott <tubbo@psychedeli.ca>
2013-04-26 13:41:08 -0400

    Refactor tests for Test::Unit.

--- test/lib/runtest/command_test.rb
+++ test/lib/runtest/command_test.rb
@@ -2,38 +2,38 @@ require 'test_helper'
 require 'runtest/command'
 
 module Runtest
-  class CommandTest < MiniTest::Unit
-    subject { Command.new '' }
-
-    test "throw an error when the example to run is blank" do
-      assert_throws FileNotFound, subject
+  class CommandTest < Test::Unit::TestCase
+    should "throw an error when the example to run is blank" do
+      assert_raises FileNotFound do
+        @command = Command.new "not/a/real/file.rb"
+      end
     end
 
     context "when running a full test file" do
-      subject { Command.new "test/lib/runtest/command_test.rb" }
+      setup { @command = Command.new "test/lib/runtest/command_test.rb" }
 
       should "run all examples in the file" do
-        assert_equal "test/lib/runtest/command_test.rb", subject.file_name
+        assert_equal "test/lib/runtest/command_test.rb", @command.file_name
       end
 
       should "resolve to a blank line number" do
-        assert_equal 0, subject.line_number
+        assert_equal 0, @command.line_number
       end
     end
 
     context "when specifying a line number in a test file" do
-      subject { Command.new "test/lib/runtest/command_test.rb", 6 }
+      setup { @command = Command.new "test/lib/runtest/command_test.rb:15" }
 
       should "find the file correctly" do
-        assert_equal "test/lib/runtest/command_test.rb", command.file_name
+        assert_equal "test/lib/runtest/command_test.rb", @command.file_name
       end
 
       should "resolve to the correct line number" do
-        assert_equal 6, command.line_number
+        assert_equal 14, @command.line_number
       end
 
       should "find the closest example to said line number" do
-        assert_equal "run all examples in a test file", command.closest_example
+        assert_equal "run all examples in the file", @command.closest_example
       end
     end
   end
